5bfa34a2ff0381ab2892316c873b0c44753889f8
ami-ne001 <ami-ne001@users.noreply.github.com>
2025-12-01 23:09:31 +0100

    Classes Commande, DetailsCommande added

---
+++ PharmaPlus/Commande.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace PharmaPlus
+{
+    internal class Commande
+    {
+        public int ID_Commande { get; set; }
+        public int ID_Utilisateur { get; set; }
+        public DateTime DateCommande { get; set; }
+        public decimal MontantTotal { get; set; }
+
+        public List<DetailsCommande> Details { get; set; } = new List<DetailsCommande>();
+
+        public void InsererCommande()
+        {
+            using (SqlConnection conn = Connection.GetConnexion())
+            {
+                string query = @"INSERT INTO Commandes (ID_Utilisateur, DateCommande, MontantTotal)
+                                 VALUES (@ID_Utilisateur, @DateCommande, @MontantTotal);
+                                 SELECT CAST(SCOPE_IDENTITY() AS INT);";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@ID_Utilisateur", ID_Utilisateur);
+                    cmd.Parameters.AddWithValue("@DateCommande", DateCommande);
+                    cmd.Parameters.AddWithValue("@MontantTotal", MontantTotal);
+
+                    ID_Commande = (int)cmd.ExecuteScalar();
+                }
+            }
+
+            // Ajouter les détails après création
+            foreach (var d in Details)
+            {
+                d.ID_Commande = ID_Commande;
+                d.InsererDetail();
+            }
+        }
+
+        public void MettreAJourCommande()
+        {
+            using (SqlConnection conn = Connection.GetConnexion())
+            {
+                string query = @"UPDATE Commandes SET 
+                                 ID_Utilisateur = @ID_Utilisateur,
+                                 DateCommande = @DateCommande,
+                                 MontantTotal = @MontantTotal
+                                 WHERE ID_Commande = @ID_Commande";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@ID_Utilisateur", ID_Utilisateur);
+                    cmd.Parameters.AddWithValue("@DateCommande", DateCommande);
+                    cmd.Parameters.AddWithValue("@MontantTotal", MontantTotal);
+                    cmd.Parameters.AddWithValue("@ID_Commande", ID_Commande);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void SupprimerCommande()
+        {
+            using (SqlConnection conn = Connection.GetConnexion())
+            {
+                string deleteDetails = "DELETE FROM DetailsCommande WHERE ID_Commande = @ID_Commande";
+                string deleteCommande = "DELETE FROM Commandes WHERE ID_Commande = @ID_Commande";
+
+                using (SqlCommand cmd1 = new SqlCommand(deleteDetails, conn))
+                using (SqlCommand cmd2 = new SqlCommand(deleteCommande, conn))
+                {
+                    cmd1.Parameters.AddWithValue("@ID_Commande", ID_Commande);
+                    cmd2.Parameters.AddWithValue("@ID_Commande", ID_Commande);
+
+                    cmd1.ExecuteNonQuery();
+                    cmd2.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static List<Commande> ListerCommandes()
+        {
+            var liste = new List<Commande>();
+
+            using (SqlConnection conn = Connection.GetConnexion())
+            {
+                string query = "SELECT * FROM Commandes ORDER BY DateCommande DESC";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        liste.Add(new Commande
+                        {
+                            ID_Commande = Convert.ToInt32(reader["ID_Commande"]),
+                            ID_Utilisateur = Convert.ToInt32(reader["ID_Utilisateur"]),
+                            DateCommande = Convert.ToDateTime(reader["DateCommande"]),
+                            MontantTotal = Convert.ToDecimal(reader["MontantTotal"])
+                        });
+                    }
+                }
+            }
+
+            return liste;
+        }
+
+        public void ChargerDetails()
+        {
+            Details = DetailsCommande.ListerDetailsParCommande(ID_Commande);
+        }
+    }
+}
---
+++ PharmaPlus/DetailsCommande.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace PharmaPlus
+{
+    internal class DetailsCommande
+    {
+        public int ID_Detail { get; set; }
+        public int ID_Commande { get; set; }
+        public int ID_Medicament { get; set; }
+        public int Quantite { get; set; }
+        public decimal PrixUnitaire { get; set; }
+
+        public void InsererDetail()
+        {
+            using (SqlConnection conn = Connection.GetConnexion())
+            {
+                string query = @"INSERT INTO DetailsCommande 
+                                 (ID_Commande, ID_Medicament, Quantite, PrixUnitaire)
+                                 VALUES (@ID_Commande, @ID_Medicament, @Quantite, @PrixUnitaire)";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@ID_Commande", ID_Commande);
+                    cmd.Parameters.AddWithValue("@ID_Medicament", ID_Medicament);
+                    cmd.Parameters.AddWithValue("@Quantite", Quantite);
+                    cmd.Parameters.AddWithValue("@PrixUnitaire", PrixUnitaire);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void MettreAJourDetail()
+        {
+            using (SqlConnection conn = Connection.GetConnexion())
+            {
+                string query = @"UPDATE DetailsCommande SET
+                                 ID_Medicament = @ID_Medicament,
+                                 Quantite = @Quantite,
+                                 PrixUnitaire = @PrixUnitaire
+                                 WHERE ID_Detail = @ID_Detail";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@ID_Detail", ID_Detail);
+                    cmd.Parameters.AddWithValue("@ID_Medicament", ID_Medicament);
+                    cmd.Parameters.AddWithValue("@Quantite", Quantite);
+                    cmd.Parameters.AddWithValue("@PrixUnitaire", PrixUnitaire);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void SupprimerDetail()
+        {
+            using (SqlConnection conn = Connection.GetConnexion())
+            {
+                string query = "DELETE FROM DetailsCommande WHERE ID_Detail = @ID_Detail";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@ID_Detail", ID_Detail);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static List<DetailsCommande> ListerDetailsParCommande(int idCommande)
+        {
+            List<DetailsCommande> details = new List<DetailsCommande>();
+
+            using (SqlConnection conn = Connection.GetConnexion())
+            {
+                string query = "SELECT * FROM DetailsCommande WHERE ID_Commande = @ID";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@ID", idCommande);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            details.Add(new DetailsCommande
+                            {
+                                ID_Detail = Convert.ToInt32(reader["ID_Detail"]),
+                                ID_Commande = Convert.ToInt32(reader["ID_Commande"]),
+                                ID_Medicament = Convert.ToInt32(reader["ID_Medicament"]),
+                                Quantite = Convert.ToInt32(reader["Quantite"]),
+                                PrixUnitaire = Convert.ToDecimal(reader["PrixUnitaire"])
+                            });
+                        }
+                    }
+                }
+            }
+
+            return details;
+        }
+    }
+}
